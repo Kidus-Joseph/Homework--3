8b90fe7d775bef0154b79f74434637519dd1ca28
Kidus Joseph <josephkidus@gmail.com>
2022-06-14 21:34:33 -0700

    Homework #3

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './screens/Home';
-import Message from './screens/Message';
+import Home from './screens/Home/index.jsx';
+import Message from './screens/Message/index.jsx';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
